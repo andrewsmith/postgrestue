c636a18e50fe88170976e74dbd8154df80f775ae
Andrew Smith <andrew@velvet.software>
2024-08-04 14:52:08 -0700

    Also convert scheduler to a class

--- src/postgrestue/scheduler.py
+++ src/postgrestue/scheduler.py
@@ -11,35 +11,38 @@ from .common import get_connection
 logger = logging.getLogger(__name__)
 
 
-async def move_scheduled_jobs_to_running(conn):
-    while True:
-        now = datetime.now(timezone.utc)
-        async with conn.transaction():
-            async with conn.cursor() as cur:
-                await cur.execute(
-                    """
-                    DELETE FROM scheduled_job WHERE schedule_time <= %s RETURNING job_id
-                    """,
-                    params=(now,)
-                )
-                scheduled_jobs = await cur.fetchmany()
-                await cur.executemany(
-                    """
-                    INSERT INTO running_job (job_id, attempt, state) VALUES (%s, 1, 'ENQUEUED')
-                    """,
-                    params_seq=scheduled_jobs
-                )
-                logger.info("Marked %s jobs as ready to run", len(scheduled_jobs))
-        await asyncio.sleep(5)
+class Scheduler:
+    def __init__(self, conn):
+        self.conn = conn
+
+    async def run(self):
+        logger.info("Starting scheduler...")
+        while True:
+            now = datetime.now(timezone.utc)
+            async with self.conn.transaction():
+                async with self.conn.cursor() as cur:
+                    await cur.execute(
+                        """
+                        DELETE FROM scheduled_job WHERE schedule_time <= %s RETURNING job_id
+                        """,
+                        params=(now,)
+                    )
+                    scheduled_jobs = await cur.fetchmany()
+                    await cur.executemany(
+                        """
+                        INSERT INTO running_job (job_id, attempt, state) VALUES (%s, 1, 'ENQUEUED')
+                        """,
+                        params_seq=scheduled_jobs
+                    )
+                    logger.info("Marked %s jobs as ready to run", len(scheduled_jobs))
+            await asyncio.sleep(5)
 
 
 async def main(args):
     logging.basicConfig(level=logging.DEBUG)
 
-    logger.info("Starting scheduler...")
-
     async with await get_connection() as conn:
-        await move_scheduled_jobs_to_running(conn)
+        await Scheduler(conn).run()
 
 
 if __name__ == "__main__":
