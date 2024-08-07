77af85eec485c23d427324fcef86bbf16c4c7d52
Andrew Smith <andrew@velvet.software>
2024-08-06 23:05:30 -0700

    Add test for blocked jobs

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -106,3 +106,52 @@ async def test_scheduled_job(db):
     # Attempting to process another job results in no jobs being found
     found_job = await worker.process_one_job()
     assert not found_job
+
+
+async def test_blocked_job(db):
+    client = Client(db)
+
+    owner_id = uuid1()
+    arguments1 = dict(name="Andrew")
+    description1 = JobDescription(
+        owner_id,
+        "say_hello",
+        arguments1,
+        1,
+        timedelta(minutes=1),
+        None,
+        None,
+        None,
+    )
+    job_id1 = await client.enqueue(description1)
+    assert job_id1
+
+    arguments2 = dict(name="Nick")
+    description2 = JobDescription(
+        owner_id,
+        "say_hello",
+        arguments2,
+        1,
+        timedelta(minutes=1),
+        None,
+        job_id1,  # Job 2 waits for Job 1
+        None,
+    )
+    job_id2 = await client.enqueue(description2)
+    assert job_id2
+
+    # Run the worker with a mock
+    say_hello_mock = Mock()
+    functions = dict(say_hello=say_hello_mock)
+    worker = Worker(db, functions)
+
+    # See that the first job is run correctly
+    found_job = await worker.process_one_job()
+    assert found_job
+    say_hello_mock.assert_called_once_with(**arguments1)
+    say_hello_mock.reset_mock()
+
+    # See that the second job is run correctly
+    found_job = await worker.process_one_job()
+    assert found_job
+    say_hello_mock.assert_called_once_with(**arguments2)
